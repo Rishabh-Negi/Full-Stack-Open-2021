4303b83ce9f69cba0bb68f363b245d09e273e24a
Rishabh_Negi <nrishabh20@gmail.com>
2021-07-02 02:30:48 +0530

    part2-phonebook-2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/filter'
 import PersonForm from './components/form'
 import Persons from './components/person'
+import axios from 'axios'
+
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+
+  const hooks = () => {
+    console.log('api call')
+    axios.get('http://localhost:3001/persons').then((response) => {
+      console.log(response.data)
+      setPersons(response.data)
+    })
+  }
+  useEffect(hooks, [])
 
   return (
     <div>
