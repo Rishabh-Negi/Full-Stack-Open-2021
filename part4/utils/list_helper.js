a4881d031f6fb5fa668ccc4d824bf3834806e448
Rishabh_Negi <nrishabh20@gmail.com>
2021-10-31 20:57:06 +0530

    part4-BlogList-4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,4 +6,22 @@ const totalLikes = (blogs) => {
     return blogs.reduce((p, c) => p + c.likes, 0)
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+    let favBlog = blogs.reduce((p, c) => {
+        if (p.likes > c.likes)
+            return p
+        return c
+    }, {})
+
+    return {
+        title: favBlog.title,
+        author: favBlog.author,
+        likes: favBlog.likes
+    }
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog
+}
